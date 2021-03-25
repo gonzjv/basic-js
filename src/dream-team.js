60b38c353c80dd5117a251af2d06b38f770bf3e6
gonzjv <gonzjv@ya.ru>
2021-03-25 12:23:59 +0300

    init: start dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
+// let sample = ["Matt", "  Ann", "Dmitry", "Max"];
+// function createDreamTeam(members) {
 const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    // let membersClean = members.map((elem) =>
+    //   typeof elem === "string"
+    //     ? (elem = elem)
+    //     : members.splice(members.indexOf(elem), 1)
+    // );
+    // let dream = members.reduce((accu, cur) => accu + cur.split("")[0]);
+    let dream = members.map((elem) => {
+      elem = elem.replace(/\s/g, "");
+      // console.log(elem);
+      return (elem = elem.split("")[0]);
+    });
+    // console.log(sample);
+    // console.log(dream);
+    return dream.sort().join("");
+  } else {
+    // console.log(typeof members);
+    return false;
+  }
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+// console.log(Array.isArray(sample));
+// console.log(createDreamTeam(sample));
