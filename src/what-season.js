207c81ee6b3e2257eb7d55ca2715110e946bf5e7
gonzjv <gonzjv@ya.ru>
2021-03-25 19:32:24 +0300

    init: start what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+const springDate = new Date(2020, 02, 31);
+// let res = springDate.getMonth();
+// const CustomError = require("../extensions/custom-error");
+// module.exports = function getSeason(date) {
+function getSeason(date) {
+  let month = springDate.getMonth();
+  console.log(month);
+  return month <= 2 ? "winter" : month <= 5 ? "spring" : false;
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+}
+// console.log(getSeason(springdate));
+console.log(springDate);
+console.log();
+console.log(getSeason(springDate));
