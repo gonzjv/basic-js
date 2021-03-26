c35e9f63a185b25dce6c3ed37d668b1ac8d3a977
gonzjv <gonzjv@ya.ru>
2021-03-26 06:15:02 +0300

    feat: fit some requirements in what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,15 +1,40 @@
-const springDate = new Date(2020, 02, 31);
-// let res = springDate.getMonth();
-// const CustomError = require("../extensions/custom-error");
-// module.exports = function getSeason(date) {
-function getSeason(date) {
-  let month = springDate.getMonth();
-  console.log(month);
-  return month <= 2 ? "winter" : month <= 5 ? "spring" : false;
-  // throw new CustomError('Not implemented');
+// const date = new Date(2020, 08, 15);
+const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {
+  // function getSeason(date) {
+  if (Array.isArray(date)) {
+    try {
+      throw new Error("Whoops! Very tricky!");
+    } catch (e) {
+      console.error(e.name + ": " + e.message);
+    }
+  }
+  if (Object.prototype.toString.call(date) !== "[object Date]") {
+    // throw new CustomError("invalid argument");
+    try {
+      throw new Error("Whoops!");
+    } catch (e) {
+      console.error(e.name + ": " + e.message);
+    }
+  }
+  if (date == undefined) {
+    return "Unable to determine the time of year!";
+    // return null;
+  }
+  let month = date.getMonth();
+  // console.log(month);
+  console.log(date);
+  console.log(Object.prototype.toString.call(date));
+  return month <= 1
+    ? "winter"
+    : month <= 4
+    ? "spring"
+    : month <= 7
+    ? "summer"
+    : month <= 10
+    ? "autumn"
+    : "winter";
   // remove line with error and write your code here
-}
-// console.log(getSeason(springdate));
-console.log(springDate);
-console.log();
-console.log(getSeason(springDate));
+};
+// console.log("---------");
+// console.log(getSeason(date));
