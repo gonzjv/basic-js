144425f55ebc0e62e8730d9db8a0274cdd401493
gonzjv <gonzjv@ya.ru>
2021-03-28 09:29:13 +0300

    feat: complete simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,25 +2,73 @@ const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
+    return this.arr.length;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    if (!Array.isArray(this.arr)) {
+      this.arr = [];
+    }
+    value === undefined
+      ? this.arr.push("()")
+      : this.arr.push("( " + value + " )");
+    console.log("add link" + this.arr);
+    return this;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    if (
+      typeof position !== "number" ||
+      !Number.isInteger(position) ||
+      position > this.arr.length ||
+      position <= 0
+    ) {
+      this.arr.splice(0, this.arr.length);
+      throw new Error();
+      // try {
+      //   throw new Error("Whoops! Position is invalid!");
+      // } catch (err) {
+      //   console.error(err.name + ": " + err.message);
+      // }
+    } else {
+      console.log(position);
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    this.arr.reverse();
+    return this;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    let chain = this.arr.join("~~");
+    this.arr.splice(0, this.arr.length);
+    console.log(chain);
+    return chain;
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }
+  },
 };
 
 module.exports = chainMaker;
+// console.log(
+//   chainMaker
+//     .addLink(1)
+//     .addLink(2)
+//     .addLink()
+//     .addLink(3)
+//     .removeLink(3)
+//     .finishChain()
+// );
+// console.log(
+//   chainMaker.addLink(1).addLink(2).addLink(3).reverseChain().finishChain()
+// );
+// console.log(
+//   chainMaker.addLink(1).addLink(2).addLink(3).reverseChain().getLength()
+// );
